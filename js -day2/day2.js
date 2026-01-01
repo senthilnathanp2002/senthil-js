03bf36a7d7e2ef7c5746468ff4bddb6cc0578db5
senthil2409surya <senthilnathanp2002@gmail.com>
2026-01-01 23:30:18 +0530

    js-day2

---
+++ js -day2/day2.js
@@ -0,0 +1,36 @@
+// let num = -5;
+
+// if (num > 0) {
+//   console.log("Positive number");
+// } else if (num < 0) {
+//   console.log("Negative number");
+// } else {
+//   console.log("Zero");
+// }
+// let marks = 75;
+
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 75) {
+//   console.log("Pass");
+// } else if (marks >= 40) {
+//   console.log("Just Pass");
+// } else {
+//   console.log("Fail");
+// }
+
+// let userName = "admin";
+// let password = "1234";
+
+// if (userName === "admin" && password === "1234") {
+//   console.log("Valid User");
+// } else {
+//   console.log("Invalid User");
+// }
+// let price = "99.99";
+
+// let convertedPrice = parseFloat(price);
+
+// console.log(convertedPrice);          
+// console.log(typeof convertedPrice);  
+
