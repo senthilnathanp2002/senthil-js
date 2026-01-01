6338510e3a4dc32461c23d8b4870b04e64fe82ba
senthil2409surya <senthilnathanp2002@gmail.com>
2026-01-01 23:19:08 +0530

    js aassignment
    js assignment

---
+++ senthil/script.js
@@ -0,0 +1,22 @@
+// let result = 2 + 3 * 4;
+// console.log(result === 14); // true
+// let marks = 55;
+
+// let status = marks >= 40 ? "Pass" : "Fail";
+// console.log(status);
+// let marks = 55;
+// let count = 5;
+// let newCount = ++count;
+
+// console.log(count);    
+// console.log(newCount); 
+// let a = 10;
+// let b = 0;      
+// let c = "JS";
+
+// let result = a && b && c;
+// console.log(result); 
+// let a=null;
+// let b=null;
+// console.log(a == 0);   
+// console.log(b === 0); 
